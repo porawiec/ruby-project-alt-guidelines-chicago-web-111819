6784bb193b5a61324f9ef2f872ef9181e6df538f
github username <github email address>
2019-12-06 12:27:16 -0600

    Successfully added average values to coffee list.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.sqlite')
 require_all 'lib'
+
+ActiveRecord::Base.logger = nil
--- lib/coffee.rb
+++ lib/coffee.rb
@@ -4,23 +4,37 @@ class Coffee < ActiveRecord::Base
     belongs_to :user
 
     def reviews
-        tastings = self.tastings.select do |tasting|
-            tasting.review
-        end
+        tastings = self.tastings.select {|tasting| tasting.review}
         tastings.map {|x| x.review}
     end
 
     def self.coffee_options
 
         Coffee.all.each { |i|
-            if i.milk == 0 && i.foam == 0
-                puts "#{i.ctype}\n #{i.shots} espresso shot(s)\n without steamed milk\n without foam\n\n"
-            elsif i.milk == 1 && i.foam == 0
-                puts "#{i.ctype}\n #{i.shots} espresso shot(s)\n with steamed milk\n without foam\n\n"
-            elsif i.milk == 0 && i.foam == 1
-                puts "#{i.ctype}\n #{i.shots} espresso shot(s)\n without steamed milk\n with foam\n\n"
-            else i.milk == 1 && i.foam == 1
-                puts "#{i.ctype}\n #{i.shots} espresso shot(s)\n with steamed milk\n with foam\n\n"
+            if i.reviews.count > 0
+                coffee_review_array = i.reviews.map{|x| x.rating}
+                sum = coffee_review_array.inject{ |sum, x| sum + x }
+                average = (sum / i.reviews.count)
+
+                if i.milk == 0 && i.foam == 0
+                    puts "#{i.ctype} --- Average rating: #{average}\n #{i.shots} espresso shot(s)\n without steamed milk\n without foam\n\n"
+                elsif i.milk == 1 && i.foam == 0
+                    puts "#{i.ctype} --- Average rating: #{average}\n #{i.shots} espresso shot(s)\n with steamed milk\n without foam\n\n"
+                elsif i.milk == 0 && i.foam == 1
+                    puts "#{i.ctype} --- Average rating: #{average}\n #{i.shots} espresso shot(s)\n without steamed milk\n with foam\n\n"
+                else i.milk == 1 && i.foam == 1
+                    puts "#{i.ctype} --- Average rating: #{average}\n #{i.shots} espresso shot(s)\n with steamed milk\n with foam\n\n"
+                end
+            else
+                if i.milk == 0 && i.foam == 0
+                    puts "#{i.ctype} --- Average rating: Not yet rated\n #{i.shots} espresso shot(s)\n without steamed milk\n without foam\n\n"
+                elsif i.milk == 1 && i.foam == 0
+                    puts "#{i.ctype} --- Average rating: Not yet rated\n #{i.shots} espresso shot(s)\n with steamed milk\n without foam\n\n"
+                elsif i.milk == 0 && i.foam == 1
+                    puts "#{i.ctype} --- Average rating: Not yet rated\n #{i.shots} espresso shot(s)\n without steamed milk\n with foam\n\n"
+                else i.milk == 1 && i.foam == 1
+                    puts "#{i.ctype} --- Average rating: Not yet rated\n #{i.shots} espresso shot(s)\n with steamed milk\n with foam\n\n"
+                end
             end
         }
     end
